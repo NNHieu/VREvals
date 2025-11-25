bc791b5243500282e0dee0726623eda84a1c2e96
NNHieu <ngochieutb13@gmail.com>
2025-11-25 03:30:02 -0500

    scan job dir

--- spawn_sampling.sh
+++ spawn_sampling.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 # List of GPUs you want to use
-GPUS=(0 1 2 3 4 5 7)
+GPUS=(0 1 6 7)
 # GPUS=(0)
 
-# Loop through GPUs and launch a process on each
-for GPU in "${GPUS[@]}"; do
-    echo "Starting process on GPU $GPU"
-    CUDA_VISIBLE_DEVICES=$GPU python run_sampling.py \
-        --job_dir runs/default/math500.qwen-1.5b-inst \
-        --sampler_config_dir distilled-150.prefix/ThinkTo \
-        --gpu_id $GPU \
-        --n 5 &
-done
+JOB_DIR=runs/default/aime25.qwen2.5-math-7b
 
-echo "Launched all processes!"
-wait
+# Search all sampler_config_dir in job dir and start sample
+
+# Find all directories at depth 2 from $JOB_DIR that match "sample_*"
+SAMPLE_DIRS=($(find "$JOB_DIR" -mindepth 2 -maxdepth 2 -type d -name "sample_*" | sort))
+
+for SAMPLE_DIR in "${SAMPLE_DIRS[@]}"; do
+    for GPU in "${GPUS[@]}"; do
+        echo "Starting sampling for $SAMPLE_DIR on GPU $GPU"
+        # Get the sampler_config_dir as the path relative to $JOB_DIR
+        SAMPLER_CONFIG_DIR=$(realpath --relative-to="$JOB_DIR" "$SAMPLE_DIR")
+        CUDA_VISIBLE_DEVICES=$GPU python run_sampling.py \
+            --job_dir $JOB_DIR \
+            --sampler_config_dir "$SAMPLER_CONFIG_DIR" \
+            --gpu_id $GPU \
+            --n 256 &
+    done
+    echo "Launched all processes!"
+    wait
+done
